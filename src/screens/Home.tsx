0c1ee470d2a907eba26e95752ca66e28694d06cd
Desenvolvimento FCP <guiguischuch@gmail.com>
2024-08-21 16:47:27 -0300

    Fix - Permissions

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,7 +4,10 @@ import {
   StyleSheet
 } from "react-native";
 
-import { check, request, PERMISSIONS, RESULTS, checkNotifications } from 'react-native-permissions';
+// Locartion
+import * as Location from "expo-location";
+import * as TaskManager from "expo-task-manager";
+
 
 // Hooks
 import { useEffect, useState } from "react";
@@ -19,17 +22,64 @@ import { lightTheme, defaultTheme } from "src/utils/theme";
 import { useSelector } from "react-redux";
 
 const Home = () => {
-  const { localDefined } = useSelector((state: any) => state.pedal);
-  const [locationEnabled, setLocationEnabled] = useState(false);
-  
-  useEffect(() => {
-    // const checkLocationPermission = async () => {
-      
-    // };
+  const { localDefined } = useSelector((state: any) => state.pedal);  
+  const [currentLocation, setCurrentLocation] = useState<Location.LocationObject | null>(null);
 
-    // checkLocationPermission();
+  const BACKGROUND_LOCATION = "background-location-task";
+
+  TaskManager.defineTask(BACKGROUND_LOCATION, ({ data, error }) => {
+    if (error) {
+      console.error("Error in location task:", error);
+      return;
+    }
+    if (data) {
+      const { locations }: any = data;
+      if (locations && locations.length > 0) {
+        const location = locations[0];        
+        handleLocation(location);
+      }
+    }
+  });
+
+  const handleLocation = (location: Location.LocationObject) => {
+    console.log(location);
+    setCurrentLocation(location);
+  }
+
+  useEffect(() => {
+    const requestPermissions = async () => {
+      const { status: foregroundStatus } = await Location.requestForegroundPermissionsAsync();
+      const { status: backgroundStatus } = await Location.requestBackgroundPermissionsAsync();
+      
+      return { foregroundStatus, backgroundStatus }
+    }
     
-    
+    requestPermissions().then(async (response) => {
+      const { foregroundStatus, backgroundStatus } = response;
+      console.info(`foregroundStatus: ${foregroundStatus}`);
+      console.info(`backgroundStatus: ${backgroundStatus}`);
+
+      if (foregroundStatus === "granted" && backgroundStatus === "granted") {
+        await Location.startLocationUpdatesAsync(BACKGROUND_LOCATION, {
+          accuracy: Location.Accuracy.BestForNavigation,
+          timeInterval: 60000, 
+          distanceInterval: 0,
+          showsBackgroundLocationIndicator: true,
+          foregroundService: {
+            notificationTitle: "Localicação sendo compartilhada.",
+            notificationBody: "Sua localização está sendo utilizada em segundo plano por esse aplicativo.",
+          },
+        });
+      }
+
+      if(!currentLocation) {
+        Location.getCurrentPositionAsync({
+          accuracy: Location.Accuracy.BestForNavigation
+        }).then((response) => {
+          setCurrentLocation(response);
+        })
+      }
+    })
   }, []);
 
   return (
@@ -40,7 +90,11 @@ const Home = () => {
         )}
 
         {localDefined === false && (
-          <Text>oie</Text>
+          currentLocation ? (
+            <Text>Lat: {currentLocation.coords.latitude}, Lon: {currentLocation.coords.longitude}</Text>
+          ) : (
+            <Text>oie</Text>
+          )
         )}
       </View>
     </View>
